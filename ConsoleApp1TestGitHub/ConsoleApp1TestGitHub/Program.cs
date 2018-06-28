0c81671bb1c896835135bb797dda2b543db66dcc
DESKTOP-FH35OGC\mugurel <mugurel.rata@duk-tech.com>
2018-06-28 17:14:56 +0300

    branch1 commit

--- ConsoleApp1TestGitHub/ConsoleApp1TestGitHub/Program.cs
+++ ConsoleApp1TestGitHub/ConsoleApp1TestGitHub/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApp1TestGitHub
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("line 0-0");
             Console.WriteLine("line 1-322");
             Console.WriteLine("line 1-321");
             Console.WriteLine("line 2-32");
@@ -24,6 +25,7 @@ namespace ConsoleApp1TestGitHub
             Console.WriteLine("line 8-11");
             Console.WriteLine("line 9-11");
             Console.WriteLine("line 10-11");
+            Console.WriteLine("line 12-11");
         }
     }
 }
