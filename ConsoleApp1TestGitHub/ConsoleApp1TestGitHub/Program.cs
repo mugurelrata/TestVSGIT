fc285b8084a5b107f9fb0796842f403060da487d
DESKTOP-FH35OGC\mugurel <mugurel.rata@duk-tech.com>
2018-06-28 12:03:45 +0300

    initial commit

---
+++ ConsoleApp1TestGitHub/ConsoleApp1TestGitHub/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1TestGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("line 1-1");
+            Console.WriteLine("line 2-1");
+            Console.WriteLine("line 3-1");
+            Console.WriteLine("line 4-1");
+            Console.WriteLine("line 5-1");
+            Console.WriteLine("line 6-1");
+            Console.WriteLine("line 7-1");
+            Console.WriteLine("line 8-1");
+        }
+    }
+}
